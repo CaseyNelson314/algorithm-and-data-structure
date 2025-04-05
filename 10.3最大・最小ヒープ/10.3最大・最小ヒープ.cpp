6f5aa21cc6f69ee447a42f53ffb2cdacbeca3271
Okawa Yusuke <caseynelson314@gmail.com>
2025-04-05 20:19:40 +0900

    update

---
+++ 10.3最大・最小ヒープ/10.3最大・最小ヒープ.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <sstream>
+#include <vector>
+
+
+int main()
+{
+    // input:
+    // 10
+    // 4 1 3 2 16 9 10 14 8 7
+    // req:
+    // 16 14 10 8 7 9 3 2 4 1
+
+    using namespace std::string_literals;
+
+    std::istringstream iss{ R"(10
+4 1 3 2 16 9 10 14 8 7
+)" };
+
+    int n;
+    iss >> n;
+
+    std::vector<int> A(n + 1);
+    for (int i = 1; i <= n; ++i)
+        iss >> A[i];
+
+    auto max_heapify = [&](auto self, int curr) -> void
+        {
+            int left = curr * 2;
+            int right = curr * 2 + 1;
+
+            // 親、左右の子の中で最大のノードを選択する
+            int largest = curr;
+            if (left <= n && A[left] > A[largest])
+                largest = left;
+            if (right <= n && A[right] > A[largest])
+                largest = right;
+
+            // 親が最大の場合はすでに最大ヒープになっているので何もしない
+            if (largest == curr) return;
+
+            std::swap(A[curr], A[largest]);
+            self(self, largest);  // スワップによって左側の最大ヒープ構造が壊れたので再構築
+        };
+
+    for (int i = n / 2; i >= 1; --i)
+    {
+        max_heapify(max_heapify, i);
+    }
+
+    for (int i = 1; i <= n; ++i)
+    {
+        std::cout << A[i] << " ";
+    }
+
+    std::cout << std::endl;
+}
---
+++ 10.4優先度付きキュー/10.4優先度付きキュー.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+#include <sstream>
+#include <vector>
+
+
+
+int A[1024 + 1];
+
+void insert(int key)
+{
+    int parent = std::floor(key / 2);
+}
+
+void extract()
+{
+
+}
+
+template <typename T>
+T read(std::istream& is)
+{
+    T v;
+    is >> v;
+    return v;
+}
+
+int main()
+{
+    std::istringstream iss{ R"(
+insert 8
+insert 2
+extract
+insert 10
+extract
+insert 11
+extract
+extract
+end
+)" };
+
+
+
+    for (;;)
+    {
+        auto s = read<std::string>(iss);
+
+        if (s == "end")
+        {
+            break;
+        }
+
+        if (s == "insert")
+        {
+            int v = read<int>(iss);
+        }
+
+        else if (s == "extract")
+        {
+
+        }
+
+        else
+        {
+            std::cerr << "invalid command" << std::endl;
+        }
+
+
+    }
+}
