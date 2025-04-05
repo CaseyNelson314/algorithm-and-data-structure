828565c0dafbf1df59cbc555f5b3e51b85216d2a
Okawa Yusuke <caseynelson314@gmail.com>
2025-04-05 16:44:58 +0900

    update

--- 9.4二分探索木：削除/9.4二分探索木：削除.cpp
+++ 9.4二分探索木：削除/9.4二分探索木：削除.cpp
@@ -4,19 +4,15 @@
 struct Node
 {
     int key;
+    Node* parent;
     Node* right;
     Node* left;
 };
 
-Node* root;
+Node* root = new Node{ -1, nullptr, nullptr, nullptr };  // NIL
 
 void insert(int key)
 {
-    if (not root)
-    {
-        root = new Node{ key, nullptr, nullptr };
-        return;
-    }
 
     Node* curr = root;
     for (;;)
@@ -27,7 +23,7 @@ void insert(int key)
                 curr = curr->left;
             else
             {
-                curr->left = new Node{ key, nullptr, nullptr };
+                curr->left = new Node{ key, curr, nullptr, nullptr };
                 break;
             }
         }
@@ -37,7 +33,7 @@ void insert(int key)
                 curr = curr->right;
             else
             {
-                curr->right = new Node{ key, nullptr, nullptr };
+                curr->right = new Node{ key, curr, nullptr, nullptr };
                 break;
             }
         }
@@ -71,23 +67,100 @@ void inorder(Node* root)
     inorder(root->right);
 }
 
-bool serch(Node* root, int key)
+Node* find(Node* root, int key)
 {
     if (not root)
-        return false;
+        return nullptr;
 
-    if (key == root->key)
-        return true;
+    if (root->key == key)
+        return root;
 
     if (key < root->key)
-        return serch(root->left, key);
+        return find(root->left, key);
 
-    return serch(root->right, key);
+    return find(root->right, key);
 }
 
-void del(Node* root, int key)
+bool serch(Node* root, int key)
 {
-    
+    return find(root, key);
+}
+
+void del(int key)
+{
+    Node* del_node = find(root, key);
+
+    if (not del_node)
+        return;
+
+    // 削除対象が子を持たない場合、親ノードの管理下から切り離す
+    if (not del_node->left && not del_node->right)
+    {
+        std::cout << "削除対象が子を持たない場合" << std::endl;
+        if (del_node->parent->right == del_node)
+            del_node->parent->right = nullptr;
+        else if (del_node->parent->left == del_node)
+            del_node->parent->left = nullptr;
+        delete del_node;
+        return;
+    }
+
+    // 削除対象が1つの子を持つ場合、削除対象の親と、削除対象の子を接続する
+    if (del_node->left && not del_node->right)
+    {
+        std::cout << "削除対象が左のみに子を持つ場合" << std::endl;
+        if (del_node->parent->right == del_node)
+            del_node->parent->right = del_node->left;
+        else if (del_node->parent->left == del_node)
+            del_node->parent->left = del_node->left;
+        del_node->left->parent = del_node->parent;
+        delete del_node;
+        return;
+    }
+
+    // 削除対象が1つの子を持つ場合、削除対象の親と、削除対象の子を接続する
+    if (not del_node->left && del_node->right)
+    {
+        std::cout << "削除対象が右のみに子を持つ場合" << std::endl;
+        if (del_node->parent->right == del_node)
+        {
+            del_node->parent->right = del_node->right;
+        }
+        else if (del_node->parent->left == del_node)
+        {
+            del_node->parent->left = del_node->right;
+        }
+        del_node->right->parent = del_node->parent;
+        delete del_node;
+        return;
+    }
+
+
+    std::cout << "削除対象が左右に子を持つ場合" << std::endl;
+
+    // 削除対象が2つの子を持つ場合、削除対象の右の子を親としたときの最小値ノードを昇格する。
+    // つまり削除対象の右の子から見て一番左側のノードを昇格させる
+    Node* min_node = del_node->right;
+    while (min_node->left)
+    {
+        min_node = min_node->left;
+    }
+
+    // 最小値ノードの親の管理下から切断
+    if (min_node->parent->left == min_node)
+        min_node->parent->left = nullptr;
+    else if (min_node->parent->right == min_node)
+        min_node->parent->right = nullptr;
+
+    // 削除対象と右の最小値ノードを入れ替える
+    min_node->left = del_node->left;
+    min_node->right = del_node->right;
+    min_node->parent = del_node->parent;
+    if (del_node->parent->left == del_node)
+        del_node->parent->left = min_node;
+    else if (del_node->parent->right == del_node)
+        del_node->parent->right = min_node;
+    delete del_node;
 }
 
 int main()
@@ -95,30 +168,33 @@ int main()
     using namespace std::string_literals;
 
     std::istringstream iss{ R"(18
-insert 8
-insert 2
-insert 3
-insert 7
-insert 22
-insert 1
-find 1
-find 2
-find 3
-find 4
-find 5
-find 6
-find 7
-find 8
-print
-delete 3
-delete 7
-print
-)" };
+    insert 8
+    insert 2
+    insert 3
+    insert 7
+    insert 22
+    insert 1
+    find 1
+    find 2
+    find 3
+    find 4
+    find 5
+    find 6
+    find 7
+    find 8
+    print
+    delete 3
+    delete 7
+    print
+    )" };
+
+    //auto& iss = std::cin;
 
     int n;
     iss >> n;
 
     for (int i = 0; i < n; ++i)
+        //for (;;)
     {
         std::string s;
         iss >> s;
@@ -130,9 +206,9 @@ print
         }
         else if (s == "print")
         {
-            std::cout << "inorder" << std::endl;
-            inorder(root);
-            std::cout << std::endl;
+            //std::cout << "inorder" << std::endl;
+            //inorder(root);
+            //std::cout << std::endl;
 
             std::cout << "preorder" << std::endl;
             preorder(root);
@@ -148,7 +224,11 @@ print
         {
             int key;
             iss >> key;
-            del(root, key);
+            del(key);
+        }
+        else
+        {
+            std::cout << "invalid command" << std::endl;
         }
     }
 }
---
+++ 9.5標準ライブラリによる集合の管理/9.5標準ライブラリによる集合の管理.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <string>
+#include <set>
+
+template <typename T>
+T read()
+{
+    T v;
+    std::cin >> v;
+    return v;
+}
+
+int main()
+{
+
+    std::set<int> set;
+
+    for (;;)
+    {
+        auto s = read<std::string>();
+
+        if (s == "ins")
+        {
+            set.insert(read<int>());
+        }
+
+        else if (s == "del")
+        {
+            set.erase(read<int>());
+        }
+
+        else if (s == "find")
+        {
+            auto pos = set.find(read<int>());
+            if (pos != set.end())
+                std::cout << "found" << std::endl;
+            else
+                std::cout << "not found" << std::endl;
+        }
+
+        else if (s == "print")
+        {
+            for (auto&& it : set)
+            {
+                std::cout << it << std::endl;
+            }
+        }
+
+        else
+        {
+            std::cout << "invalid command" << std::endl;
+        }
+    }
+}
+
